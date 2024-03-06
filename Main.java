cf6a85da0c66aaecf85aaa4170c47f100eb68809
Fandy <casperadam91@gmail.com>
2024-03-06 09:45:15 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         String pass = "testingP@ssWORD123";
         String auth = "YzNyc3BkNzZmZGNtczNiMjhtdGc6UjctX1ZOVEZQaU92WWxCbHFBVjdpUXhFLXVTQzBfWjFqWE1fZS0tOEZZZw==";
+
+
+        System.out.println("eyJraWQiOiI3MjUxMzM0ZS1hNTUzL");
     }
 
 }
